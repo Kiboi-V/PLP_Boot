e66c45a4399195dd23e481392d19496f87050f74
Kiboi-V <115711939+Kiboi-V@users.noreply.github.com>
2024-03-08 09:44:18 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,5 @@
+name=input(str('Enter name :'))
+age=int(input(("Enter your age :")))
+location=input("Enter Location :")
+
+print ("Hello {} you are {} years old and live in {}".format(name,age,location))
